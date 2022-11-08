008375d9bd2cbba81c81422fb1dbaeb8ae298204
anamulislamshamim <anamulislamshamim117@gmail.com>
2022-11-08 08:13:27 +0600

    completed experience and stat component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../About/About';
 import Header from '../Header/Header';
+import Statistic from '../Statistic/Statistic';
 
 const Home = () => {
     return (
         <>
         <Header />
         <About />
+        <Statistic  />
         </>
     );
 };
